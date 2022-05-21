e26f2a555d5f2af230e845258c98ce5d23079a44
TheoPeri <theo.perinet@epita.fr>
2022-05-22 00:39:03 +0200

    add final notebook

--- stock.py
+++ stock.py
@@ -1,10 +1,19 @@
 from pyspark.sql import SparkSession
-import pandas as pd
 
-from pyspark.sql.functions import col, isnan, when, count
-from pyspark.sql.functions import mean
-from pyspark.sql.functions import col, asc, desc
-from pyspark.sql.functions import monotonically_increasing_id
+import pyspark.sql.functions as func
+from pyspark.sql.functions import (
+    col,
+    isnan,
+    when,
+    count,
+    mean,
+    lag,
+    isnull,
+    datediff,
+    asc,
+    desc,
+    monotonically_increasing_id
+)
 from pyspark.sql.types import (
     DoubleType,
     IntegerType,
@@ -13,14 +22,15 @@ from pyspark.sql.types import (
     StructType,
     StructField,
 )
-from pyspark.sql.functions import desc
-import pyspark.sql.functions as func
 
 
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
-from pyspark.sql import functions as F
 from pyspark.sql.window import Window
+
+# only used to print informations
+import pandas as pd
+import numpy as np
 
 spark_application_name = "WannaFlop_Project"
 spark = SparkSession.builder.appName(spark_application_name).getOrCreate()
@@ -28,17 +38,28 @@ spark = SparkSession.builder.appName(spark_application_name).getOrCreate()
 
 class Stock(object):
     def __init__(self, file_path, header=False, delimiter=";", schema=None):
+        # save attributs
         self.file_path = file_path
         self.header = header
         self.delimiter = delimiter
         self.schema = schema
 
+        # load the file and save the Spark DataFrame
         self.df = self._load_df()
 
+        # save the Explore and Analysis objects of the current stock
         self.explore = self.Explore(self)
         self.analysis = self.Analysis(self)
+        
+    def print_name(self):
+        # print the name of the stock
+        name = self.file_path.split(".")[-2].split("/")[-1]
+        print("###########" + "#" * len(name) + "###########")
+        print("########## " + name + " ##########")
+        print("###########" + "#" * len(name) + "###########\n")
 
     def _get_num_cols(self):
+        # get column names with double or integer type
         num_cols = [
             f.name
             for f in self.df.schema.fields
@@ -48,24 +69,15 @@ class Stock(object):
         return num_cols
 
     def _get_rounded_df(self):
-        rounded_df = self.df
-        dbl_cols = self._get_num_cols()
-        for col in dbl_cols:
-            rounded_df = rounded_df.withColumn(col, func.round(col))
+        # round each double/integer values of each columns
+        df = self.df
+        for col in self._get_num_cols():
+            df = df.withColumn(col, func.round(col))
 
-        return rounded_df
-
-    def _get_periodicity(self):
-        self.df["data"][0]
+        return df
 
     def _handle_csv(self):
-        """
-        @description: Read the csv file and return a Spark DataFrame
-
-        @arg csv_file_path: Path to the csv file
-        @arg header: boolean whether to load a header or not
-        @arg delimiter: which delimiter to use by default
-        """
+        # Read the csv file and return a Spark DataFrame
         return (
             spark.read.option("inferSchema", "true")
             .option("nullValue", "null")
@@ -78,31 +90,31 @@ class Stock(object):
         )
 
     def _handle_json(self):
-        return spark.read.json(self.file_path)
+        # Read the json file and return a Spark DataFrame
+        return spark.read.schema(self.schema).json(self.file_path)
 
     def _load_df(self):
-        ####### ADD TRY CATCH #####
-        extension = self.file_path.split(".")[-1]
-
+        # load the file depending of the extension
         df = None
+        
+        extension = self.file_path.split(".")[-1] if "." in self.file_path else ""
         if extension == "json":
             df = self._handle_json()
         elif extension == "csv":
             df = self._handle_csv()
+        else:
+            raise Exception("Current file format is not handled yet. We support csv and json formats.")
 
         return df
 
     class Explore:
-        """
-        Need to add:
-            - Correlation
-            - period (already done just need to add it)
-        """
 
         def __init__(self, stock):
+            # save attributs
             self.stock = stock
 
         def __repr__(self):
+            # print every infos on the stock
             return f"""
             {self._print_schema()}\n
             {self.get_df_abstract()}\n
@@ -111,10 +123,12 @@ class Stock(object):
             {self.get_missing()}"""
 
         def get_missing(self):
+            # count the number of missing data on each colums
             print("Missing Data per column:")
             self._count_missing().show()
 
         def get_df_abstract(self):
+            # print the first and last 40 rows of the stock
             rounded_df = self.stock._get_rounded_df()
 
             # First 40 rows
@@ -126,6 +140,7 @@ class Stock(object):
             rounded_df.orderBy(desc("Date")).show(40)
 
         def get_stats(self):
+            # get stat for each columns
             rounded_df = self.stock.df
             summary = rounded_df.summary()
             cols = self.stock._get_num_cols()
@@ -134,20 +149,57 @@ class Stock(object):
             print("Stock Stats:")
             summary.show()
 
-        def _nb_rows(self):
+        def nb_rows(self):
             # Number of total rows
             print("Number of rows: " + str(self.stock.df.count()) + "\n")
+
+        def correlation(self):
+            # print correlation between each double/int columuns
+            cols = self.stock._get_num_cols()
+            
+            res = pd.DataFrame(1, index=cols, columns=cols, dtype=np.double)
+            
+            for x in range(len(cols)):
+                for y in range(x + 1, len(cols)):
+                    col_1, col_2 = cols[x], cols[y]
+                    cor = self.stock.df.stat.corr(col_1, col_2)
+                    res.loc[col_1, col_2] = cor
+                    res.loc[col_2, col_1] = cor
+            
+            print(res)
+            print("")
+
+        def period(self):
+            # get the period between data points : "day", "week", "month", "year"
+            my_window = Window.partitionBy().orderBy("Date")
+            
+            df = self.stock.df
+            df = df.withColumn("prev_value", lag(df.Date).over(my_window))
+            df = df.withColumn("diff", when(isnull(datediff(df.Date, df.prev_value)), 0)
+                               .otherwise(datediff(df.Date, df.prev_value)))
+            mean_diff = df.select(mean('diff')).first()[0]
+            
+            if mean_diff < 4:
+                return "day"
+            elif mean_diff < 8:
+                return "week"
+            elif mean_diff < 40:
+                return "month"
+            return "year"
+        
+        def print_period(self):
+            print("Period: " + self.period())
 
         def _print_schema(self):
             self.stock.df.printSchema()
 
         def _count_missing(self):
+            # count the number of missing values
             cols = self.stock.df.columns
             cols.remove("Date")
             return self.stock.df.select(
                 [count(when(isnan(c), c)).alias(c) for c in cols]
             )
-            # .show()
 
     class Analysis:
         def __init__(self, stock):
@@ -155,22 +207,39 @@ class Stock(object):
             self.df = stock.df
 
         def get_oc_avg(self, period):
-            """HANDLE BY WEEK"""
+            # get average opening / closing price
             close = self._compute_avg(self.df, "Close", period)
             opening = self._compute_avg(self.df, "Open", period)
 
+            if period == "week":
+                return (
+                    close.join(opening, (opening.Date == close.Date) & (opening.Week_number == close.Week_number), "inner")
+                    .select(close.Date, close.Week_number, close.Close_mean, opening.Open_mean)
+                    .orderBy("Date", "Week_number")
+                )
             return (
                 close.join(opening, opening.Date == close.Date, "inner")
                 .select(close.Date, close.Close_mean, opening.Open_mean)
                 .orderBy("Date")
             )
+        
+        def print_get_oc_avg(self):
+            # print average opening / closing price for each period
+            for period in ["week", "month", "year"]:
+                self.get_oc_avg(period).show()
 
         def get_price_change(self, period=None):
             df = self.df
             if period:
                 df = self.get_oc_avg(period)
+                return df.withColumn("diff", (df["Close_mean"] - df["Open_mean"]))
 
-            return df.withColumn("diff", (df["Close_mean"] - df["Open_mean"]))
+            return df.withColumn("diff", (df["Close"] - df["Open"]))
+        
+        
+        def print_price_change(self):
+            for period in ["day", "month"]:
+                self.get_price_change(period).show()
 
         def get_daily_return_rate(self, period="day", start_price=None, nb_shares=1):
             df = self.get_price_change(period)
@@ -194,6 +263,18 @@ class Stock(object):
             return df.select(func.max('diff')).first()[0]
 
         def _compute_avg(self, df, col, period):
+            # compute the average value of a column by a period
+            if period == "week":
+               # for week, we une the number of the week 
+                df = (
+                    df.withColumn("Week_number", func.weekofyear(func.col("Date")))
+                    .withColumn("Date", func.date_format(func.col("Date"), "yyyy"))
+                    .groupBy("Date", "Week_number")
+                    .agg(mean(col).alias(col + "_mean"))
+                    .orderBy("Date", "Week_number")
+                )
+                return df
+            
             date = {"day": "yyyy-MM-dd", "month": "yyyy-MM", "year": "yyyy"}
             df = (
                 df.withColumn("Date", func.date_format(func.col("Date"), date[period]))
--- stocks.py
+++ stocks.py
@@ -3,16 +3,23 @@ from stock import Stock
 import glob
 
 class Stocks(object):
-    def __init__(self, files=None, schema=None): 
-        self.stocks = self._load_stocks(files, schema)
+    def __init__(self, files=None, header=False, delimiter=";", schema=None):
+        
+        # load and save stocks
+        self.stocks = self._load_stocks(files, header, delimiter, schema)
 
-    def _load_stocks(self, files, schema):
+    def _load_stocks(self, files, header, delimiter, schema):
+        # load stocks contained in files
+        
+        # use all the csv files in stocks_data/ if files argument not provided
         if not files:
             files = glob.glob("stocks_data/*.csv")
 
         dfs = []
         for file in files:
-            dfs.append(Stock(file, header=True, delimiter=',', schema=schema))
+            # call the Stock class to load each stock/file
+            dfs.append(Stock(file, header=header, delimiter=delimiter,
+                             schema=schema))
         
         return dfs
 
@@ -23,4 +30,17 @@ class Stocks(object):
             max_daily_returns[cmpny_name] = (stock.analysis.get_daily_return_max())
 
         return dict(sorted(max_daily_returns.items(), key=lambda item: item[1], reverse=True))
-
+    
+    def call_explore_function(self, function_name):
+        # call a specific function to each explore object of each stock
+        for stock in self.stocks:
+            stock.print_name()
+            func = getattr(stock.explore, function_name)
+            func()
+    
+    def call_analysis_function(self, function_name):
+        # call a specific function to each analysis object of each stock
+        for stock in self.stocks:
+            stock.print_name()
+            func = getattr(stock.analysis, function_name)
+            func()
