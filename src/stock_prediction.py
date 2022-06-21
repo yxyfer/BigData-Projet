de9bec3a196dc51cfc97db1614c1d2c0aed1fce3
mathieu.rivier <mathieu.rivier@epita.fr>
2022-06-21 13:56:24 +0200

    [ADD] - rmse

--- src/stock_prediction.py
+++ src/stock_prediction.py
@@ -36,7 +36,9 @@ class StockPrediction(object):
         self.testDF = None
 
     def _train_split(self):
-        self.trainDF, self.testDF = self.predict.fullDF.randomSplit([0.8, 0.2], seed=42)
+        self.trainDF, self.testDF = self.predict.fullDF.randomSplit(
+            [0.8, 0.2], seed=42
+        )
 
     def linear_regression(self):
         self._train_split()
@@ -48,7 +50,9 @@ class StockPrediction(object):
             featuresCol="features_not_scaled"
         )
 
-        scaler = MinMaxScaler(inputCol="features_not_scaled", outputCol="features")
+        scaler = MinMaxScaler(
+            inputCol="features_not_scaled", outputCol="features"
+        )
 
         lr = LinearRegression(
             labelCol="next_" + predicting_col,
@@ -70,8 +74,8 @@ class StockPrediction(object):
         predicting_col = self.predict.col_to_pred
         pred = self.exec_linear_regression()
 
-        pred.select("next_" + predicting_col, "pred_next_" + predicting_col).show()
-
+        pred.select("next_" + predicting_col,
+                    "pred_next_" + predicting_col).show()
 
     def plot_linear_regression(self):
         predicting_col = self.predict.col_to_pred
@@ -92,8 +96,12 @@ class StockPrediction(object):
         next_name = "next_" + predicting_col
         pred_next_name = "pred_next_" + predicting_col
 
-        pred = pred.withColumn("real_UP", pred[next_name] > pred[predicting_col])
-        pred = pred.withColumn("pred_UP", pred[pred_next_name] > pred[predicting_col])
+        pred = pred.withColumn(
+            "real_UP", pred[next_name] > pred[predicting_col]
+        )
+        pred = pred.withColumn(
+            "pred_UP", pred[pred_next_name] > pred[predicting_col]
+        )
         pred = pred.withColumn("pred_good", pred["real_UP"] == pred["pred_UP"])
 
         return pred
@@ -104,7 +112,10 @@ class StockPrediction(object):
 
         nb_good = pred.where(pred["pred_good"] == True).count()
 
-        print("Percent of good prediction of UP/DOWN : " + str(nb_good/ nb_value) + "%")
+        print(
+            "Percent of good prediction of UP/DOWN : " +
+            str(nb_good / nb_value) + "%"
+        )
 
     def gain_predict(self):
         pred = self.get_up_down()
@@ -112,9 +123,16 @@ class StockPrediction(object):
         predicting_col = self.predict.col_to_pred
         next_name = "next_" + predicting_col
 
-        pred = pred.withColumn("price_change", func.abs(pred[next_name] - pred[predicting_col]))
-        pred = pred.withColumn("price_sign", when(pred["pred_good"] == True, 1).otherwise(-1))
-        pred = pred.withColumn("gain", pred["price_change"] * pred["price_sign"])
+        pred = pred.withColumn(
+            "price_change", func.abs(pred[next_name] - pred[predicting_col])
+        )
+        pred = pred.withColumn(
+            "price_sign",
+            when(pred["pred_good"] == True, 1).otherwise(-1)
+        )
+        pred = pred.withColumn(
+            "gain", pred["price_change"] * pred["price_sign"]
+        )
 
         return pred
 
@@ -126,7 +144,6 @@ class StockPrediction(object):
         df.plot.bar()
         plt.show()
 
-
     def print_stonks_or_not(self):
         pred = self.gain_predict()
         gain = pred.select(func.sum("gain")).collect()[0][0]
@@ -134,4 +151,46 @@ class StockPrediction(object):
         predicting_col = self.predict.col_to_pred
         start_price = pred.select(predicting_col).first()[0]
 
-        print("With an initial buy at " + str(start_price) + "$ we won " + str(gain) + "$")
+        print(
+            "With an initial buy at " + str(start_price) + "$ we won " +
+            str(gain) + "$"
+        )
+
+    def rmse(self):
+        # Complete the evaluator code
+        predicting_col = self.predict.col_to_pred
+        evaluator = RegressionEvaluator(
+            metricName="rmse",
+            labelCol="next_" + predicting_col,
+            predictionCol="pred_next_" + predicting_col
+        )
+
+        return evaluator
+
+    def eval_rmse(self):
+        evaluator = self.rmse()
+        print(f"Metric name: {evaluator.getMetricName()}")
+        print(f"Label Column: {evaluator.getLabelCol()}")
+        print(f"Prediction column: {evaluator.getPredictionCol()}")
+
+        reg = self.exec_linear_regression()
+        RMSE = evaluator.evaluate(reg)
+
+        print(f"RMSE value: {RMSE}\n\n")
+
+    def normalised_rmse(self):
+        # Normalized RMSE = RMSE / (max value – min value)
+        val_max = self.stock.df.select(func.max(self.stock.df['Close'])
+                                      ).first()[0]
+        val_min = self.stock.df.select(func.min(self.stock.df['Close'])
+                                      ).first()[0]
+
+        print(f"Max Value: {val_max}")
+        print(f"Min Value: {val_min}")
+
+        evaluator = self.rmse()
+        reg = self.exec_linear_regression()
+        RMSE = evaluator.evaluate(reg)
+
+        normalised_rmse = RMSE / (val_max - val_min)
+        print(f"Normalised RMSE : {normalised_rmse}\n\n")
